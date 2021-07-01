a0c49b321b87a4d2701e4a193196276ede61d92a
agunaco <agunaco3994@gmail.com>
2021-07-01 18:52:21 +0900

    laravel

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+//모델은 하나의 레코드다. 
 class Post extends Model
 {
+    //protected $table = 'my_posts'; 테이블 지정 
     use HasFactory;
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/posts/create', [PostsController::class, 'create']);
+//Route::get('/posts.create', 'PostsController@create');
+
+Route::post('/posts/store', [PostsController::class, 'store']);
+//get post 방식 두개 
+//get방식은 정보 조회할 때
+//post 방식은 자원에 대한 변경방식.
+//게시글 등록 삭제 내용 변경 등은 post방식으로 하기. 해킹의 악용가능. 
+//model 대응되는 table의 하나의 레코드를 나타내는 클래스를 정의.
+
+Route::get('/posts/index', [PostsController::class, 'index']);
