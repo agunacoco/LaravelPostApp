11a776f66f4a0e049dcf88649ee9b5d2d21cd5d1
agunacoco <agunaco3994@gmail.com>
2021-07-08 16:56:24 +0900

    agunacoco

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,10 +12,16 @@ class Post extends Model
     //protected $table = 'my_posts'; 테이블 지정 
     use HasFactory;
 
+
     public function imagePath()
     {
         $path = env('IMAGE_PATH', '/storage/images/');
         $imageFile = $this->image ?? 'nono.jpg';
         return $path . $imageFile;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class); // function으로 user를 부를 수 있다. 
     }
 }
